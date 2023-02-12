a850fedbeff15757b2a491dfdec5cfb22ae5af89
Rohan Rah Mudvari <mudvari25@gmail.com>
2023-02-12 07:40:12 +0545

    updates

--- lib/IntroPage.dart
+++ lib/IntroPage.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:switchingscreen/main.dart';
 
 class IntroPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+   return Scaffold(
+     appBar: AppBar(
+       title: Text("Intro"),
+     ),
+     body:
+     Center(
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+
+
+         children: [
+           Text("Welcome", style: TextStyle( fontWeight: FontWeight.bold, fontSize: 34),),
+           SizedBox( height: 11,),
+           ElevatedButton(onPressed: (){
+             Navigator.push(context, MaterialPageRoute(builder:(context){
+               return MyHomePage();
+             }));
+           }, child: Text("Next")),
+         ],
+       ),
+     ),
+   );
   }
-  
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:switchingscreen/IntroPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,16 +8,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello"),
-        ),
-        body: Column(
-          children: [
-            Text("Hello World"),
-          ],
-        ),
+      home: IntroPage(),
+
+    );
+  }
+
+}
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Classico'),
+
       ),
+      body: Text("Hello World",style: TextStyle( fontWeight: FontWeight.bold, fontSize: 34),),
     );
   }
 }
